27ddcec2c62932df02cfc6d4c7e7bd1df379c262
Sai Reddy <sai.reddy@target.com>
2019-11-10 23:08:45 -0600

    added cross origin

--- src/main/java/com/bitbyte/cargaraag/auth/controllers/AuthController.java
+++ src/main/java/com/bitbyte/cargaraag/auth/controllers/AuthController.java
@@ -9,16 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 
 @RestController
+@CrossOrigin
 public class AuthController {
 
     private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
